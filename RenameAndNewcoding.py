8e013641d3461d4993ee5420a0058bea4077d763
Jeremyywb <gjeremyyou@gmail.com>
2017-05-02 16:01:44 +0800

    Update RenameAndNewcoding.py

--- RenameAndNewcoding.py
+++ RenameAndNewcoding.py
@@ -16,3 +16,15 @@ for docname in to_rename:
     path_doc = "E:/path/to/file/"+docname
     path_rename = "E:/another_or_sanme_path/to/file/"+newnames
     os.rename(path_doc,path_rename)
+
+def genSQL(col,tbname,fname,dbname):
+    createsql_0 = 'create table ['+ dbname +'].[dbo].['
+    createsql_1 = ']( '
+    colstr = ',\n'.join([co + ' VARCHAR(MAX)' for co in col]) + ')'
+    sql = createsql_0 +tbname+createsql_1 + colstr
+    Bucksql = 'bulk insert [' + dbname + '].[dbo].[' + tbname + "] from 'E:/自动化存储源/准备导入数据库/" + fname + "'"+''' with(   
+   FIRSTROW=2,
+   FIELDTERMINATOR='\t',   
+   ROWTERMINATOR='\n'  
+)  '''
+    return sql,Bucksql
